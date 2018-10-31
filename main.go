7f92742bce15ffac809e82f00247ee8e9bcabf3c
Duong <mrhayxinhun@gmail.com>
2018-10-31 18:54:19 +0700

    update

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"dev.hocngay.com/hocngay/compile-test/handler"
 	"dev.hocngay.com/hocngay/compile-test/model"
 	"github.com/gin-gonic/gin"
+	"github.com/panjf2000/ants"
 )
 
 var m sync.Mutex
@@ -21,8 +23,15 @@ func main() {
 	handler.InitCreCont(&queue)
 
 	fmt.Println(queue.Go)
+	// Test with worker pool
+	p, _ := ants.NewPoolWithFunc(15, func(i interface{}) error {
+		handleTest(i.(*gin.Context))
+		return nil
+	})
 
-	r.GET("/test", handleTest)
+	r.GET("/test", func(ctx *gin.Context) {
+		p.Serve(ctx)
+	})
 
 	//Run service
 	port := os.Getenv("PORT")
@@ -60,9 +69,25 @@ func handleTest(ctx *gin.Context) {
 	// <-ch
 
 	// C2: tạo sẵn 5 container cho từng ngôn ngữ
-	ch := make(chan string, 20)
-	for i := 0; i < 20; i++ {
-		go handler.HandlerCompile2("go", `
+	// ch := make(chan string, 20)
+	// for i := 0; i < 20; i++ {
+	// 	go handler.HandlerCompile2("go", `
+	// 	package main\n
+	// 	import "fmt"\n
+	// 	func main() {\n
+	// 		sum := 0\n
+	// 		for i := 0; i < 10; i++ {\n
+	// 			sum += i\n
+	// 		}\n
+	// 		fmt.Println(sum)\n
+	// 	}`, ch, &queue, &m)
+	// }
+	// <-ch
+
+	//C3 Pool worker
+	ch := make(chan string, 1)
+	startTime := time.Now()
+	go handler.HandlerCompile3("go", `
 		package main\n
 		import "fmt"\n
 		func main() {\n
@@ -71,9 +96,9 @@ func handleTest(ctx *gin.Context) {
 				sum += i\n
 			}\n
 			fmt.Println(sum)\n
-		}`, ch, &queue, &m)
+		}`, &queue, &m, ch)
+	<- ch	
 
-	}
-	<-ch
-
+	totalTime := time.Since(startTime)
+	fmt.Println("Time 3:", totalTime)
 }
